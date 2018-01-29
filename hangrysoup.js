4e0fce86bf65ed0a0726e2db737374ec4344969b
Phillip Clapham <phillip@claphamtech.com>
2018-01-28 21:21:42 -0500

    Further styling and layout

---
+++ hangrysoup.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function soupTurn() {
+  let letter = document.getElementById("hangInput");
+  alert(hangInput.value);
+}
+
+let answerText = document.getElementById("answerTextId");
+answerText.innerText = "_ _ _ _ _ _";
